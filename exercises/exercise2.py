b1c038a8cc773c133d6828b32570c7e585066cc1
Joaquin <joaquinguerrero256@gmail.com>
2024-04-03 21:20:51 -0300

    subo ejercicios del 5 al 9

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -26,7 +26,7 @@ lado_cuadrado = 5
 area_cuadrado = pow(lado_cuadrado, 2)
 
 # COMPLETAR - INICIO
-area_mayor_a_cinco = not (area_cuadrado <= 5)
+area_mayor_a_cinco = not (area_cuadrado <= lado_cuadrado)
 # COMPLETAR - FIN
 
 assert area_mayor_a_cinco
